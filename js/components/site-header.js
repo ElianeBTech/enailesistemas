f12a92b1b4d1cb2b38831033c3df7d384852e281
ElianeB <li_brs@hotmail.com>
2026-07-26 18:55:31 -0300

    Update site-header.js

--- js/components/site-header.js
+++ js/components/site-header.js
@@ -2,9 +2,9 @@ class SiteHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML =
       `<header class="topo">
-   <div class="logo"> <img src="../assets/image/logo.png"/></div> 
+   <div class="logo"> <img src="../../assets/image/logo.png"/></div> 
         <nav class="navegacao">
-        <a href="/index.html">Home</a>
+        <a href="../index.html">Home</a>
         <a href="#">Serviços</a>
         <a href="https://elianebtech.github.io/enailesistemas/js/pages/login.html">Login</a>
         <a href="https://elianebtech.github.io/enailesistemas/js/pages/cadastro.html">Cadastro</a>
